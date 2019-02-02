74d8b3f7dc5ab2bf46852f7d9ba934c1a7dd2cd1
Kelvin D. Meeks <kmeeks@intltechventures.com>
2019-02-02 14:07:45 -0800

    updated file

---
+++ examples/Demo-001/Demo-001.jl
@@ -0,0 +1,12 @@
+"Demo of Julia String handling"
+
+"Char handling example code"
+test_001 = 'a'
+println(Int(test_001))
+println(Char(97))
+
+
+"String handling example code"
+test_002 = "Monday"
+println(test_002[1:2])
+
